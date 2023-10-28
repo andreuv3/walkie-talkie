e8a34e43d6039a65c7e6aec0685bfce5eb7535be
André <andre@uvfranchising.com.br>
2023-10-27 21:06:38 -0300

    criando estrutura base do programa, com arquivo de configurações

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,19 @@
-﻿
-Console.WriteLine("Bem-vindo ao Walkie Talkie =)");
+﻿using Microsoft.Extensions.Configuration;
+
+try
+{
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", false, false)
+        .Build();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu um erro inesperado durante a execução do programa: {ex.Message}");
+    Console.WriteLine(ex.StackTrace);
+
+    if (ex.InnerException != null)
+    {
+        Console.WriteLine($"Mais detalhes do erro: {ex.InnerException.Message}");
+        Console.WriteLine(ex.InnerException.StackTrace);
+    }
+}
